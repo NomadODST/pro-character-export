0a5af17a7dd3619775183242182faf0d2b2457e3
NomadODST <konstantin.zahn@googlemail.com>
2026-03-06 01:17:15 +0100

    Create item-parser.js

---
+++ scripts/item-parser.js
@@ -0,0 +1,83 @@
+export function parseActorItems(actor){
+
+const weapons = [];
+const spells = [];
+const feats = [];
+const equipment = [];
+const tools = [];
+
+for(const item of actor.items){
+
+switch(item.type){
+
+case "weapon":
+weapons.push(parseWeapon(actor,item));
+break;
+
+case "spell":
+spells.push(parseSpell(item));
+break;
+
+case "feat":
+feats.push(item.name);
+break;
+
+case "equipment":
+case "loot":
+case "consumable":
+equipment.push(`${item.name} x${item.system.quantity || 1}`);
+break;
+
+case "tool":
+tools.push(item.name);
+break;
+
+}
+
+}
+
+spells.sort((a,b)=>a.level-b.level);
+
+return {
+weapons,
+spells,
+feats,
+equipment,
+tools
+};
+
+}
+
+function parseWeapon(actor,item){
+
+const sys = actor.system;
+
+const ability = item.system.abilityMod || "str";
+
+const mod = sys.abilities[ability].mod;
+
+const prof = item.system.proficient ? sys.attributes.prof : 0;
+
+const magic = Number(item.system.attackBonus || 0);
+
+const attackBonus = mod + prof + magic;
+
+const dmg = item.system.damage.parts?.[0];
+
+return {
+name:item.name,
+attack:attackBonus,
+damage: dmg ? `${dmg[0]} ${dmg[1]}` : ""
+};
+
+}
+
+function parseSpell(item){
+
+return {
+name:item.name,
+level:item.system.level,
+prepared:item.system.preparation?.prepared
+};
+
+}
